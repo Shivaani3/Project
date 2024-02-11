cc1881ea0172c47b5cdf0ffba9ea7daf6a3bbafc
Shivaani3 <148319526+Shivaani3@users.noreply.github.com>
2024-02-11 23:22:19 +0800

    Create Main.java
    TicTacToe with GUI. Youtube reference (Bro Code)

---
+++ tictactoeGUI/Main.java
@@ -0,0 +1,12 @@
+package tictactoegui;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+        TicTacToeGUI tictactoe = new TicTacToeGUI();
+       
+    }
+}
